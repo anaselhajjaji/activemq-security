46cf29a635650ae53b717fd44982ca426fce36bd
Anas El Hajjaji <anas.el-hajjaji@exfo.com>
2020-07-06 19:32:09 -0400

    init csharp projects

---
+++ activemq-producer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace activemq_producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
